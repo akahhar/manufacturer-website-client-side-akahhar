7d11027e0f9ea0192a67d05d2990c0a4fda1b2c0
unknown <sm.hasanur121@gmail.com>
2022-05-26 15:01:10 +0600

    error fix

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -17,7 +17,7 @@ const MyOrders = () => {
     isLoading,
     refetch,
   } = useQuery("orders", () =>
-    fetch(`http://localhost:5000/orders?email=${user.email}`, {
+    fetch(`http://localhost:5000/orders/${user.email}`, {
       method: "GET",
       headers: {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
